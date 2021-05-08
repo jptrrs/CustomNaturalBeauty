bddb33b7de66a6ec36ab623ec2497ba41a5d5548
João Pedro Torres <jaopedro05@outlook.com>
2021-05-08 19:13:12 -0300

    Yanky filter implemented.

--- Source/Base.cs
+++ Source/Base.cs
@@ -6,13 +6,17 @@ using RimWorld;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using System.Reflection;
+using HarmonyLib;
 
 namespace CustomNaturalBeauty
 {
     public class Base : ModBase
     {
+        public static string SearchField = "";
+        public static Dictionary<SettingHandle, bool> Matched = new Dictionary<SettingHandle, bool>();
+        private static List<SettingHandle> allHandles = new List<SettingHandle>();
         internal static Base Instance { get; private set; }
-
         public override string ModIdentifier
         {
             get { return "CustomNaturalBeauty"; }
@@ -39,8 +43,9 @@ namespace CustomNaturalBeauty
         {
             var plants = DefDatabase<ThingDef>.AllDefs.Where(x => x.plant != null).OrderBy(x => x.label);
             var chunks = DefDatabase<ThingDef>.AllDefs.Where(x => x.defName.StartsWith("Chunk")).OrderBy(x => x.label);
+            var rocks = DefDatabase<ThingDef>.AllDefs.Where(x => x.mineable).OrderBy(x => x.label);
             var terrain = DefDatabase<TerrainDef>.AllDefs.OrderBy(x => x.label);
-            IEnumerable<BuildableDef> affected = plants.Cast<BuildableDef>().Concat(chunks.Cast<BuildableDef>()).Concat(terrain.Cast<BuildableDef>());
+            IEnumerable<BuildableDef> affected = plants.Cast<BuildableDef>().Concat(chunks.Cast<BuildableDef>()).Concat(rocks.Cast<BuildableDef>()).Concat(terrain.Cast<BuildableDef>());
 
             var source = Settings.GetHandle("DefaultsFrom", "DefaultsFromTitle".Translate(), "DefaultsFromDesc".Translate(), ProfileEnum.BaseGame, null, "profile");
             source.OnValueChanged = newvalue =>
@@ -57,6 +62,13 @@ namespace CustomNaturalBeauty
                 ResetValues();
             };
 
+            var searchField =  Settings.GetHandle<string>("SearchField", "SearchField", "SearchField", "");
+            searchField.OnValueChanged = searchWord =>
+            {
+                MatchSearch(searchWord);
+            };
+            SearchField = searchField;
+
             foreach (BuildableDef e in affected)
             {
                 bool hasBeauty = e.statBases != null && e.statBases.StatListContains(StatDefOf.Beauty);
@@ -65,22 +77,45 @@ namespace CustomNaturalBeauty
                 int defBeauty = hasBeauty ? (int)e.statBases.First((StatModifier s) => s.stat == StatDefOf.Beauty).value : 0;
                 BaseBeauty.Add(e.defName, defBeauty);
                 int defaultValue = isPreset ? presetValue : defBeauty;
-                //if (isPreset) Log.Message(e.defName + " is preset, presetValue is "+presetValue+", defBeauty is "+defBeauty+", assigned " + defaultValue);
                 if (!hasBeauty)
                 {
                     if (e.statBases == null) e.statBases = new List<StatModifier>();
                     e.statBases.Add(new StatModifier() { stat = StatDefOf.Beauty, value = defaultValue });
                 }
                 var customBeauty = Settings.GetHandle<int>(e.defName, e.label, e.description, defaultValue, Validators.IntRangeValidator(-50, +50));
-                //RegisteredBeauty.Add(e.defName, customBeauty.Value);
                 customBeauty.OnValueChanged = newValue =>
                 {
-                    //RegisteredBeauty[e.defName] = newValue;
                     e.SetStatBaseValue(StatDefOf.Beauty, newValue);
-                    //Log.Message(e.defName + " beauty has changed to " + newValue);
+                    ResetControl(customBeauty);
                 };
+                allHandles.Add(customBeauty);
                 e.SetStatBaseValue(StatDefOf.Beauty, customBeauty.Value);
             }
+        }
+
+        private void MatchSearch(string word)
+        {
+            Log.Warning($"Searching {word}");
+            foreach (var handle in allHandles)
+            {
+                handle.VisibilityPredicate = () => handle.Title.Contains(word);
+                ResetControl(handle);
+            }
+
+
+            //if (SearchField.Count() > 1)
+            //{
+            //    int length = Matched.EnumerableCount();
+            //    for (int i = 0; i < length; i++)
+            //    {
+            //        var key = Matched.ElementAt(i).Key;
+            //        Matched[key] = key.Contains(SearchField);
+            //    }
+            //}
+            //else
+            //{
+            //    foreach (var e in Matched.Keys) Matched[e] = true;
+            //}
         }
 
         private void SetNewDefaults(string key, ProfileEnum newvalue)
@@ -90,7 +125,7 @@ namespace CustomNaturalBeauty
                 SettingHandle<int> handle = h as SettingHandle<int>;
                 if (handle != null)
                 {
-                    if (newvalue > 0)
+                    if (newvalue  > 0)
                     {
                         int presetValue = 0;
                         bool isPreset = Extracted(key, handle.Name, out presetValue);
@@ -119,6 +154,12 @@ namespace CustomNaturalBeauty
             }
         }
 
+        public void ResetControl(SettingHandle hanlde)
+        {
+            MethodInfo ResetHandleControlInfo = AccessTools.Method("HugsLib.Settings.Dialog_ModSettings:ResetHandleControlInfo");
+            ResetHandleControlInfo.Invoke(Find.WindowStack.currentlyDrawnWindow, new object[] { hanlde });
+        }
+
         public static bool Button(Rect rect, SettingHandle<bool> setting, string text)
         {
             bool change = false;
@@ -130,34 +171,5 @@ namespace CustomNaturalBeauty
             }
             return change;
         }
-
-        //public static bool CustomDrawer_Button(Rect rect, SettingHandle<bool> setting, String activateText, String deactivateText, int xOffset = 0, int yOffset = 0)
-        //{
-        //    //int labelWidth = (int)rect.width - 20;
-        //    //int horizontalOffset = 0;
-        //    //int verticalOffset = 0;
-        //    //bool change = false;
-        //    //Rect buttonRect = new Rect(rect);
-        //    //buttonRect.width = labelWidth;
-        //    //buttonRect.position = new Vector2(buttonRect.position.x + horizontalOffset + xOffset, buttonRect.position.y + verticalOffset + yOffset);
-        //    //Color activeColor = GUI.color;
-        //    //bool isSelected = setting.Value;
-        //    //String text = setting ? deactivateText : activateText;
-
-        //    //if (isSelected)
-        //    //    GUI.color = SelectedOptionColor;
-        //    bool clicked = Widgets.ButtonText(buttonRect, text);
-        //    //if (isSelected)
-        //    //    GUI.color = activeColor;
-
-        //    if (clicked)
-        //    {
-        //        setting.Value = !setting.Value;
-        //        change = true;
-        //    }
-        //    return change;
-        //}
-
-
     }
 }
---
+++ Source/StatPart_PlantBeauty.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Text;
+using Verse;
+
+namespace CustomNaturalBeauty
+{
+    public class StatPart_PlantBeauty : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            Plant plant = req.Thing as Plant;
+            if (req.HasThing && plant != null)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine("plantGrowth".Translate() + ": x" + plant.Growth.ToStringByStyle(ToStringStyle.PercentOne));
+                return stringBuilder.ToString().TrimEndNewlines();
+            }
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float value)
+        {
+            Plant plant = req.Thing as Plant;
+            if (req.HasThing && plant != null)
+            {
+                value *= plant.Growth;
+            }
+        }
+            
+    }
+}
